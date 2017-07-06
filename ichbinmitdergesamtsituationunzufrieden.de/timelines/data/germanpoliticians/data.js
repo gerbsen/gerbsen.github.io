3664dfd9accce381d39f03176647b3151c6d1f3b
Daniel Gerber <gerber@motionintelligence.net>
2017-07-06 23:05:58 +0200

    german politicans quotes

---
+++ ichbinmitdergesamtsituationunzufrieden.de/timelines/data/germanpoliticians/data.js
@@ -0,0 +1,52 @@
+var data = {
+	"title": {
+		"media": {
+			"url": "../../../assets/img/politics.jpg",
+			"thumbnail" : "../../../assets/img/politics.jpg",
+			"caption": "",
+			"credit": ""
+		},
+		"text": {
+			"headline": "Deutsche Politiker",
+			"text": ""
+		}
+	},
+	"events": [{
+		"media": {
+			"url": "../../../assets/img/peter_tauber.jpg",
+			"thumbnail": "../../../assets/img/peter_tauber.jpg",
+			"credit": "<a href='http://creativecommons.org/licenses/by-sa/3.0/de/deed.en'>Tobias Koch: CC BY-SA 3.0 de</a>"
+		},
+		"start_date": {
+			"month": "07",
+			"day": "04",
+			"year": "2017"
+		},
+		"link" : "http://www.zeit.de/gesellschaft/zeitgeschehen/2017-07/cdu-generalsekretaer-peter-tauber-respektlos-tweet-minijob-twitter",
+		"tags" : ["Peter Tauber", "Minijobs", "CDU"],
+		"text": {
+			"headline": "Wenn Sie was Ordentliches gelernt haben, brauchen Sie keine drei Minijobs",
+			"text": ""
+		}
+	}]
+}
+	// {
+	// 	"media": {
+	// 		"url": "img/",
+	// 		"caption": "",
+	// 		"credit": ""
+	// 	},
+	// 	"start_date": {
+	// 		"month": "",
+	// 		"day": "",
+	// 		"year": "",
+	// 		"hour": "",
+	// 		"minute": ""
+	// 	},
+	// 	"link" : "",
+	// 	"tags" : ["North Korea", "ICBM", "Kim Jong-un"],
+	// 	"text": {
+	// 		"headline": "",
+	// 		"text": ""
+	// 	}
+	// }
