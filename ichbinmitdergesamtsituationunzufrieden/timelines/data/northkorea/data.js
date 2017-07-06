7b4bc1658ad922223f39c814de541dd5d8639b79
Daniel Gerber <gerber@motionintelligence.net>
2017-07-06 22:30:21 +0200

    first version with both nuclear maps and north korea

---
+++ ichbinmitdergesamtsituationunzufrieden/timelines/data/northkorea/data.js
@@ -0,0 +1,93 @@
+var data = {
+	"title": {
+		"media": {
+			"url": "../../../assets/img/flag_north_korea.png",
+			"thumbnail" : "../../../assets/img/flag_north_korea.png",
+			"caption": "",
+			"credit": ""
+		},
+		"text": {
+			"headline": "North Korea",
+			"text": ""
+		}
+	},
+	"events": [{
+		"media": {
+			"url": "../../../assets/img/05Nkorea2-master768.jpg",
+			"thumbnail": "../../../assets/img/05Nkorea2-master768.jpg",
+			"caption": "A photograph released by North Korea’s official news agency on Tuesday that is said to show the intercontinental ballistic missile being launched.",
+			"credit": "Credit Korean Central News Agency"
+		},
+		"start_date": {
+			"month": "07",
+			"day": "04",
+			"year": "2017"
+		},
+		"link" : "https://www.nytimes.com/2017/07/04/world/asia/north-korea-missile-test-icbm.html",
+		"tags" : ["North Korea", "ICBM", "Kim Jong-un"],
+		"text": {
+			"headline": "U.S. Confirms North Korea Fired Intercontinental Ballistic Missile",
+			"text": "SEOUL, South Korea — The Trump administration on Tuesday confirmed North Korea’s claim that it had launched an intercontinental ballistic missile, and it told Pyongyang that the United States would use “the full range of capabilities at our disposal against the growing threat.” <br/> The administration followed up that warning on Wednesday morning with a joint military exercise in which United States and South Korean forces fired ballistic missiles in the waters along the Korean Peninsula’s east coast. <br/> But North Korea reaffirmed Wednesday that it would never deviate from its determination to bolster its nuclear and missile abilities as long as the United States’ “hostile policy” and “nuclear threat” persisted."
+		}
+	},
+	{
+		"media": {
+			"url": "../../../assets/audio/BUR1445086804.mp3",
+			"caption": "Here & Now's Lisa Mullins speaks with Kathleen Stephens (@AmbStephens), former ambassador to South Korea.",
+			"credit": "WBUR Here & Now",
+			"thumbnail" : "../../../assets/img/0705_us-south-korea-1000x637.jpg"
+		},
+		"start_date": {
+			"month": "07",
+			"day": "05",
+			"year": "2017"
+		},
+		"url" : "http://www.wbur.org/hereandnow/2017/07/05/us-south-korea-kathleen-stephens",
+		"link" : "",
+		"tags" : ["North Korea", "ICBM", "Kim Jong-un", "Joint Military Exercises"],
+		"text": {
+			"headline": "U.S., South Korea Conduct Joint Military Exercises After North Korean Missile Test",
+			"text": "After North Korea announced its successful test launch of an intercontinental ballistic missile, the U.S. and South Korea responded Wednesday with joint military exercises. But it will take more that that to deescalate tensions on the Korean Peninsula. <br/><br/> Here & Now's Lisa Mullins speaks with Kathleen Stephens (@AmbStephens), former ambassador to South Korea and fellow at the Asia-Pacific Research Center at Stanford University, about the challenges."
+		}
+	},
+	{
+		"media": {
+			"url": "../../../assets/img/haley.png",
+			"thumbnail": "../../../assets/img/haley.png",
+			"caption": "Haley: North Korea launch a 'clear and sharp military escalation'",
+			"credit": "CNN politics"
+		},
+		"start_date": {
+			"month": "07",
+			"day": "05",
+			"year": "2017"
+		},
+		"url" : "http://edition.cnn.com/2017/07/05/politics/nikki-haley-north-korea-military-escalation-un/index.html",
+		"link" : "",
+		"tags" : ["North Korea", "Nicky Haley", "UN"],
+		"text": {
+			"headline": "U.S., South Korea Conduct Joint Military Exercises After North Korean Missile Test",
+			"text": "(CNN)US Ambassador to the United Nations Nikki Haley on Wednesday called North Korea's intercontinental ballistic missile launch test a \"clear and sharp military escalation\" and said US military action remained on the table.<br/><br/>She also warned China and other countries supporting North Korea that their trade with the US could be at risk if they do not stop aiding the Pyongyang regime.<br/><br/>\" The US is prepared to use the full range of our capabilities to defend ourselves and our allies,\" Haley said. \"One of our capabilities lies with our considerable military forces. We will use them if we must, but we prefer not to have to go in that direction.\""
+		}
+	}]
+}
+	// {
+	// 	"media": {
+	// 		"url": "img/",
+	// 		"caption": "",
+	// 		"credit": ""
+	// 	},
+	// 	"start_date": {
+	// 		"month": "",
+	// 		"day": "",
+	// 		"year": "",
+	// 		"hour": "",
+	// 		"minute": ""
+	// 	},
+	// 	"link" : "",
+	// 	"tags" : ["North Korea", "ICBM", "Kim Jong-un"],
+	// 	"text": {
+	// 		"headline": "",
+	// 		"text": ""
+	// 	}
+	// }
